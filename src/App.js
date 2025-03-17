021b68b30c43502068965835edc6d99d60c2bc4a
valya000999@gmail.com <valya000999@gmail.com>
2025-03-17 14:44:35 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import MovieList from "./components/MovieList";
-import MovieForm from "./components/MovieForm";
+import MovieList from "./components/MovieList/MovieList";
+import MovieForm from "./components/MovieForm/MovieForm";
 
 const App = () => {
   return (
-    <div style={{display: "flex", alignItems: "center", justifyContent: "center", gap: "50px", height: "100vh"}}>
+    <main>
       <MovieList/>
       <MovieForm/>
-    </div>
+    </main>
   );
 };
 
